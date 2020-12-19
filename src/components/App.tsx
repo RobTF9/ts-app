1c51fa420c96e28ede83433ba0c6391412732f96
Rob Squires <rsquires@sans.org>
2020-12-19 10:11:01 +0000

    prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
+import * as React from 'react';
 
-const App = () => <h1>Hello from react</h1>;
+const App = (): JSX.Element => <h1>Hello from react</h1>;
 
 export default App;
