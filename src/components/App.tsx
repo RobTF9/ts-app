84104f04b2fd5d4eb00dbd0d3256bebd2aa5f503
Rob Squires <rsquires@sans.org>
2020-12-19 10:28:33 +0000

    working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-const App = (test: any): JSX.Element => (
-  <h1>Hello from react, {test}</h1>
+const App = (test: string): JSX.Element => (
+  <h1>Hello from react, {`${test}`}</h1>
 );
 
 export default App;
